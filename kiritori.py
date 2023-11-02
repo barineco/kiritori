8f330ee387b3fb1543bb774f4731d9d91015e7b4
maughtchiner <142575857+barineco@users.noreply.github.com>
2023-11-02 22:25:55 +0900

    update for laggy actions

--- kiritori.py
+++ kiritori.py
@@ -1,13 +1,21 @@
 import os
 from atproto import Client
 from datetime import datetime, timedelta, timezone
+import time
 
 username = os.getenv('BSKY_USERNAME')
 password = os.getenv('BSKY_PASSWORD')
 
-current_time_jst = datetime.now(timezone(timedelta(hours=+9))).strftime("%H:%M")
-
 api = Client()
 api.login(username, password)
+
+JST = timezone(timedelta(hours=+9))
+wait_seconds = (60 - now.minute - 1) * 60 + (60 - now.second)
+time.sleep(max(wait_seconds, 0))
+
+while datetime.now(JST).minute != 0:
+    time.sleep(1)
+
+current_time_jst = datetime.now(JST).strftime("%H:%M")
 
 api.send_post(f'✄------------ {current_time_jst} ------------✄')
