34f0f9cc8746b293ebf534bc5c6b17e1dbd72e38
maughtchiner <142575857+barineco@users.noreply.github.com>
2023-11-02 22:17:01 +0900

    update with jst time acquisition

--- kiritori.py
+++ kiritori.py
@@ -1,10 +1,13 @@
 import os
 from atproto import Client
+from datetime import datetime, timedelta, timezone
 
 username = os.getenv('BSKY_USERNAME')
 password = os.getenv('BSKY_PASSWORD')
 
+current_time_jst = datetime.now(timezone(timedelta(hours=+9))).strftime("%H:%M")
+
 api = Client()
 api.login(username, password)
 
-api.send_post('✄------------ test ------------✄')
+api.send_post('✄------------ {current_time_jst} ------------✄')
